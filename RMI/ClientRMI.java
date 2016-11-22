a94239038dab5a818e755176f86d8dd774cfa880
Qudus Animashaun <qudusani99@gmail.com>
2016-11-22 18:51:34 +0000

    Fix: Stream error is ok now

--- RMI/ClientRMI.java
+++ RMI/ClientRMI.java
@@ -5,26 +5,63 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.*;
- 
+
 public class ClientRMI {
 
     private static String logged=null;
+    private static Replica object;
+    private static String line; 
+    private static InputStream is = null;
+    private static BufferedReader br = null;
 
-    public static void inputMethod(){
-        InputStream is = null;
-        BufferedReader br = null;
+    public static void transaction(String line){
+        try{
+            String key = line.substring(0,2);
+            switch(key){
+                case "R:":
+                System.out.print("<client>: ");
+                System.out.println(object.read());
+                System.out.print("<client>: ");
+                break;
+                case "W:":
+                System.out.print("<client>: ");
+                logged = line.substring(3);
+                object.write(line.substring(3), Values.CLIENT);
+                break;
+                default:
+                throw new Exception("\t<Invalid type entered, please choose from \"R\" for READ or \"W\"> for WRITE");
+            }
+        }catch(Exception e){
+            try{
+                Registry reg = LocateRegistry.getRegistry(1100);
+                object = (Replica) reg.lookup("Backup");
+                if(object != null)object.write(logged, Values.CLIENT);
+                inputMethod(false, line);
+            }catch(Exception p){
+
+            }
+        }
+        
+    }
+
+
+
+    public static void inputMethod(boolean token, String missedLine){
+        
         
         try {
 
             Registry reg = LocateRegistry.getRegistry(1099);
             // Client looks for the Primary node in the registry
-            System.out.println(Arrays.toString(reg.list()));
-            Replica object = (Replica) reg.lookup("Primary");
+            object = (Replica) reg.lookup("Primary");
             is = System.in;
-            br = new BufferedReader(new InputStreamReader(is));
-            System.out.println("Please enter query - 'R:' or 'W:' <item to add>'");
+            if(br== null) br = new BufferedReader(new InputStreamReader(is));
+            if(token) System.out.println("Please enter query - 'R:' or 'W:' <item to add>'");
             System.out.print("<client>: ");
-            String line = null;
+            line = null; 
+            if(!token){
+                transaction(missedLine);
+            }
             
             while (true) {
                 line = br.readLine();
@@ -32,26 +69,13 @@ public class ClientRMI {
                     if(line.length() <2){
                         throw new Exception("\t<Invalid type entered, please choose from \"R:\" for READ or \"W:\"> for WRITE");
                     }
-                    String key = line.substring(0,2);
-
                     if (line.equalsIgnoreCase("quit")) {
                         break;
                     }
 
-                    switch(key){
-                        case "R:":
-                            System.out.print("<client>: ");
-                            System.out.println(object.read());
-                            System.out.print("<client>: ");
-                            break;
-                        case "W:":
-                            System.out.print("<client>: ");
-                            logged = line.substring(3);
-                            object.write(line.substring(3), Values.CLIENT);
-                            break;
-                        default:
-                            throw new Exception("\t<Invalid type entered, please choose from \"R\" for READ or \"W\"> for WRITE");
-                    }
+                    transaction(line);
+
+                    
 
                 }else{
                     System.out.print("<client>: ");
@@ -60,14 +84,8 @@ public class ClientRMI {
             }
             
         }catch (Exception e){
-            try{
-                Registry reg = LocateRegistry.getRegistry(1100);
-                Replica object = (Replica) reg.lookup("Backup");
-                if(object != null)object.write(logged, Values.CLIENT);
-                inputMethod();
-            }catch(Exception p){
 
-            }
+            e.printStackTrace();
 
         }
         finally {
@@ -85,7 +103,7 @@ public class ClientRMI {
     }
 
     public static void main(String[] args) throws IOException {
-         inputMethod();
+       inputMethod(true, "");
 
-    }
+   }
 }
