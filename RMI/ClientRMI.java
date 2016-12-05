9babbe87ea7faada77522b59c5886ea9cf4433dc
Qudus Animashaun <qudusani99@gmail.com>
2016-12-05 00:08:09 +0000

    Comments added to Client
    Qudus

--- RMI/ClientRMI.java
+++ RMI/ClientRMI.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.net.*;
-import java.rmi.Naming; 
+import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -10,10 +10,20 @@ public class ClientRMI {
 
     private static String logged=null;
     private static Replica object;
-    private static String line; 
+    private static String line;
     private static InputStream is = null;
     private static BufferedReader br = null;
 
+    public static void main(String[] args) throws IOException {
+       inputMethod(true, "");
+   }
+
+    /**
+     *  Description: Parses the command from the client and sends it to Primary
+     *
+     *  @author Qudus Animashaun
+     *  @param  line  Client's input in the command line
+     */
     public static void transaction(String line){
         try{
             String key = line.substring(0,2);
@@ -35,22 +45,23 @@ public class ClientRMI {
             try{
                 Registry reg = LocateRegistry.getRegistry(1100);
                 object = (Replica) reg.lookup("Backup");
-                if(object != null)object.write(logged, Values.CLIENT);
+                if(object != null) object.write(logged, Values.CLIENT);
                 inputMethod(false, line);
             }catch(Exception p){
 
             }
         }
-        
     }
 
-
-
+    /**
+     *  Description: Parses the command from the client and sends it to Primary
+     *
+     *  @author Qudus Animashaun
+     *  @param  missedLine  Stores the line that did not proprogate to new Primary
+     *  @param  token  Turns false if there was a switch and last command did not proprogate
+     */
     public static void inputMethod(boolean token, String missedLine){
-        
-        
         try {
-
             Registry reg = LocateRegistry.getRegistry(1099);
             // Client looks for the Primary node in the registry
             object = (Replica) reg.lookup("Primary");
@@ -58,11 +69,10 @@ public class ClientRMI {
             if(br== null) br = new BufferedReader(new InputStreamReader(is));
             if(token) System.out.println("Please enter query - 'R:' or 'W:' <item to add>'");
             System.out.print("<client>: ");
-            line = null; 
+            line = null;
             if(!token){
                 transaction(missedLine);
             }
-            
             while (true) {
                 line = br.readLine();
                 if(!line.trim().equalsIgnoreCase("")){
@@ -72,21 +82,13 @@ public class ClientRMI {
                     if (line.equalsIgnoreCase("quit")) {
                         break;
                     }
-
                     transaction(line);
-
-                    
-
                 }else{
                     System.out.print("<client>: ");
                 }
-                
             }
-            
         }catch (Exception e){
-
             e.printStackTrace();
-
         }
         finally {
             // close the streams using close method
@@ -98,12 +100,6 @@ public class ClientRMI {
             catch (IOException ioe) {
                 System.out.println("Error while closing stream: " + ioe);
             }
-
         }
     }
-
-    public static void main(String[] args) throws IOException {
-       inputMethod(true, "");
-
-   }
 }
